0467003f41a18cf1abbd7a054f240040710bae90
epodik <epodik@gmail.com>
2019-12-11 01:20:06 +0700

    4.5. Отладка программы в IDEA[#205113]

--- chapter_001/src/test/java/ru/job4j/condition/SqAreaTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -1,26 +1,26 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
 
 public class SqAreaTest {
 
     @Test
-    public void square () {
-        int p = 6;
-        int k = 2;
-        double expected = 2;
-        double out = SqArea.square(p,k);
-        Assert.assertEquals(expected, out, 0.01);
+    public void whenp4k1s1() {
+        int k = 1;
+        int p = 4;
+        int expected = 1;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
     }
 
     @Test
-    public void square1 () {
-        int p = 50;
+    public void whenp6k2s2() {
         int k = 2;
-        double expected = 200;
-        double out = SqArea.square(p,k);
-        Assert.assertEquals(expected, out, 0.01);
+        int p = 6;
+        int expected = 2;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
     }
-
 }
---
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SqMaxTest {
+
+    @Test
+    public void whenFirstMax() {
+        int result = SqMax.max(5,4,3,2);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenSecondMax() {
+        int result = SqMax.max(1,5,3,2);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenThirdMax() {
+        int result = SqMax.max(1,4,5,2);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenForthMax() {
+        int result = SqMax.max(1,4,3,5);
+        assertThat(result, is(5));
+    }
+
+}
