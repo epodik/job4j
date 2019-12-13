fab18095c4770c5df5e3ad3ac924cd9df2d31562
epodik <epodik@gmail.com>
2019-12-13 15:52:41 +0700

    5.7. Ипотека.[#205178]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(double amount, int salary, double percent) {
+        int year = 1;
+        amount = amount + amount * percent * 0.01;
+        while (amount > salary){
+            amount = amount - salary;
+            amount = amount + amount * percent * 0.01;
+            year++;
+        }
+        return year;
+    }
+}
