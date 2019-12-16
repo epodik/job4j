ea1c26900a45f1a4d5835f97a1687cbf0d3012e3
epodik <epodik@gmail.com>
2019-12-16 23:24:09 +0700

    6.0. Заполнить массив степенями чисел.[#205119]

---
+++ chapter_001/src/main/java/ru/job4j/array/video.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class video {
+    public static void main (String[] args) {
+        int[] numbers = new int[5];
+        for (int index = 0; index != 5; index++){
+            numbers[index] = numbers.length - index;
+        }
+        for (int index = 0; index != 5; index++){
+            int value = numbers[index];
+            System.out.println(value);
+        }
+    }
+}
