74fa8fd8e871ccfce53a7405956aae5a7eef495d
epodik <epodik@gmail.com>
2019-12-19 10:14:09 +0700

    6.4. Слово начинается с ...[#205122]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -4,11 +4,11 @@ public class Check {
     public boolean mono(boolean[] data) {
         boolean result = false;
         for (int index = 1; index < data.length; index++) {
-            if (data[0] == data[index]){
-                result = true;
-            } else {
+            if (data[0] != data[index]){
                 result = false;
                 break;
+            } else {
+                result = true;
             }
         }
         return result;
