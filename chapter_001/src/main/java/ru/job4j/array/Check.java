cc329d35c347d4d20c1ceebfdea2add010f8dad0
epodik <epodik@gmail.com>
2019-12-20 09:55:39 +0700

    Исправление 6.3

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -2,13 +2,11 @@ package ru.job4j.array;
 
 public class Check {
     public boolean mono(boolean[] data) {
-        boolean result = false;
+        boolean result = true;
         for (int index = 1; index < data.length; index++) {
             if (data[0] != data[index]){
                 result = false;
                 break;
-            } else {
-                result = true;
             }
         }
         return result;
