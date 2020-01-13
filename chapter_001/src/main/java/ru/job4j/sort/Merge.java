5c2d86cb245110db99214703804fc166fb0e2fe4
epodik <epodik@gmail.com>
2020-01-13 23:49:01 +0700

    Объединить два массива [#124482]

--- chapter_001/src/main/java/ru/job4j/sort/Merge.java
+++ chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -1,26 +1,47 @@
 package ru.job4j.sort;
-
 import java.util.Arrays;
-
 public class Merge {
+
 
     public int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
         int a = 0;
         int b = 0;
-        for (int i = 0; i < rsl.length; i++) {
-            while (left[a] < right[b]) {
+        int i = 0;
+        if (left.length == 0) {
+            rsl = right;
+            i = rsl.length;
+        }
+        if (right.length == 0) {
+            rsl = left;
+            i = rsl.length;
+        }
+        boolean leftend = false;
+        boolean rightend = false;
+        while (i < rsl.length) {
+            if ((left[a] <= right[b] || rightend) && !leftend) {
                 rsl[i] = left[a];
-                a++;
+                if (a == left.length - 1) {
+                    leftend = true;
+                }
+                if (!leftend) {
+                    a++;
+                }
                 i++;
-            }
+            } else if ((right[b] <= left[a] || leftend) && !rightend) {
                 rsl[i] = right[b];
-                if (b < right.length - 1) {
+                if (b == right.length - 1) {
+                    rightend = true;
+                }
+                if (!rightend) {
                     b++;
                 }
+                i++;
+            }
         }
         return rsl;
     }
+
 
     public static void main(String[] args) {
         Merge process = new Merge();
